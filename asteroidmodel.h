348992fea90f6f11e87aeebb0ce39bc1fa86cf0b
Michal Hozza <mhozza@gmail.com>
2012-01-26 15:05:07 +0100

    Presunuta definicia MyVertex

--- asteroidmodel.h
+++ asteroidmodel.h
@@ -19,38 +19,8 @@
 #define ASTEROIDMODEL_H
 
 #include "model.h"
+#include "utils.h"
 
-struct MyVertex
-{
-  float x,y,z;
-  float tx,ty;
-  float nx, ny, nz;
-  //float padding[3];
-  MyVertex()
-  {
-    this->x = 0;
-    this->y = 0;
-    this->z = 0;
-    this->nx = 0;
-    this->ny = 0;
-    this->nz = 0;
-    this->tx = 0;
-    this->ty = 0;
-    //color = 0x9900ffff;
-  }
-  MyVertex(float x, float y, float z, float tx, float ty)
-  {
-    this->x = x;
-    this->y = y;
-    this->z = z;
-    this->nx = x;
-    this->ny = y;
-    this->nz = z;
-    this->tx = tx;
-    this->ty = ty;
-    //color = 0x9900ffff;
-  }
-};
 
 class AsteroidModel : public Model
 {
