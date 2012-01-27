e1174854cc34fc531c20b9ac2d1acfcafc56fc7a
Michal Hozza <mhozza@gmail.com>
2012-01-27 12:22:34 +0100

    Upratany kod

--- asteroidmodel.cpp
+++ asteroidmodel.cpp
@@ -15,10 +15,12 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define GL_GLEXT_PROTOTYPES
+#include "GL/gl.h"
+#include "GL/glext.h"
+
 #include "asteroidmodel.h"
 #include <cmath>
-#include "GL/gl.h"
-
 #include <cstring>
 #include <iostream>
 
@@ -29,8 +31,6 @@ using namespace std;
 AsteroidModel::AsteroidModel(int textureId)
   :Model(textureId)
 {
-  //cout << sizeof(MyVertex) << endl;
-
   float radius = 1.0;
   //create model
   //vertices
@@ -40,64 +40,20 @@ AsteroidModel::AsteroidModel(int textureId)
   for (int i = 1; i<6;i++)
   {
     int n = 4*(3-abs(i-3));
-
     float py = -sin(3*PI/2+i*PI/6)*radius;
-
     float miniradius = abs(cos(3*PI/2+i*PI/6)*radius);
     for(int j = 0;j<n;j++)
-    {
-      //float angle = j*(PI/(2*i));
+    {      
       float angle = j*(PI/(2*(3-abs(i-3))));
       float px = cos(angle)*miniradius;
       float pz = -sin(angle)*miniradius;
       MyVertex v(px,py,pz,i%2,j%2);
       vertices[ind++] = v;
-    }
-    //cout << ind << endl;
+    }    
   }
   MyVertex v1(0,-sin(PI/2)*radius,0,0,0);
   vertices[ind] = v1;
-  //cout << ind << endl;
-
-  //index
-  /*ind = 1;
-  int ind2 = 0;
-  for (int i = 1; i<6;i++)
-  {
-    int n = 4*(3-abs(i-3));
-    int np = 4*(3-abs(i-4));
-    int nn = 4*(3-abs(i-2));
-    int startind = ind;
-    for(int j = 0;j<n;j++)
-    {
-      if(i==1)
-      {
-        index[ind2++] = 0;
-      }
-      else
-      {
-        //index[ind2++] = startind+(ind-startind+1)%np;
-        index[ind2++] = 0;
-      }
-      index[ind2++] = ind;
-      index[ind2++] = startind+(j+1)%n;
-
-      index[ind2++] = ind;
-      index[ind2++] = startind+(j+1)%n;
-      if(i==6)
-      {
-        index[ind2++] = 37;
-      }
-      else
-      {
-        index[ind2++] = startind+n+2*j;
-        //index[ind2++] = startind+(ind-startind+1)%nn;
-      }
-      ind++;
-    }
-  }
-  cout << ind2 << endl;*/
-  //rucne
+  //indices
   ushort t[] = {0,1,2, 0,2,3, 0,3,4, 0,4,1, 1,5,6, 1,2,6, 1,12,5, 2,6,7, 2,7,8, 2,3,8,
                3,8,9, 3,9,10, 3,4,10, 4,10,11, 4,11,12, 4,1,12, 5,13,24, 5,13,14, 5,6,14,
                6,14,15, 6,7,15, 7,15,16, 7,16,17, 7,8,17, 8,17,18, 8,9,18, 9,18,19, 9,19,20,
@@ -111,18 +67,16 @@ AsteroidModel::AsteroidModel(int textureId)
 }
 
 void AsteroidModel::draw()
-{  
-  //glDisable(GL_TEXTURE_2D);
-  glEnableClientState(GL_VERTEX_ARRAY);
+{
+  glEnableClientState(GL_VERTEX_ARRAY);  
   glVertexPointer(3, GL_FLOAT, sizeof(MyVertex), &vertices[0].x);
   glEnableClientState(GL_NORMAL_ARRAY);
   glNormalPointer(GL_FLOAT,sizeof(MyVertex),&vertices[0].nx);
   glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-  glTexCoordPointer(2, GL_FLOAT,sizeof(MyVertex), &vertices[0].tx);
-  //glColorPointer(4, GL_UNSIGNED_BYTE, sizeof(MyVertex), &vertices[0].color);
+  glTexCoordPointer(2, GL_FLOAT,sizeof(MyVertex), &vertices[0].tx);  
   glBindTexture(GL_TEXTURE_2D,textureId);
   glDrawRangeElements(GL_TRIANGLES, 0, 37, 216, GL_UNSIGNED_SHORT, index);
-  //glDrawRangeElements(GL_TRIANGLES, 0, 37, 108, GL_UNSIGNED_SHORT, index);
-
-  //glEnable(GL_TEXTURE_2D);
+  glDisableClientState(GL_VERTEX_ARRAY);
+  glDisableClientState(GL_NORMAL_ARRAY);
+  glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 }
